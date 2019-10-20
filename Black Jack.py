58542841167fbb14ef1cd3f4ec3301eb04e73335
nameisdheeraj <55711803+nameisdheeraj@users.noreply.github.com>
2019-10-19 18:08:04 -0700

    Update Black Jack.py

--- Black Jack.py
+++ Black Jack.py
@@ -2,37 +2,28 @@
 """
 Created on Fri Oct 18 22:09:52 2019
 
-@author: namei
+@author: Dheeraj
 """
 import random
-#DEALER CARDS
-dealer_cards = []
-#PLAYER CARDS
-player_cards = []
 
-#DEAL THE CARDS
-#DEALER CARDS
+dealer_cards = []
+
+player_cards = []
 
 while len(dealer_cards) < 2:
     dealer_cards.append(random.randint(1,11))
     if len(dealer_cards) == 2:
         print("Dealer has X &", dealer_cards[1])
 
-#PLAYER CARDS
-
 while len(player_cards) < 2:
     player_cards.append(random.randint(1,11))
     if len(player_cards) == 2:
         print("You have ", player_cards)        
 
-#SUM OF PLAYER CARDS
-
 if sum(player_cards) == 21:
     print("You have a BlackJack!")
 elif sum(player_cards) > 21:
     print("You are busted") 
-
-#COMPARISION
 
 while sum(player_cards) < 21:
     print("You have a total of " + str(sum(player_cards)) + " with", player_cards)
@@ -41,7 +32,7 @@ while sum(player_cards) < 21:
         player_cards.append(random.randint(1,11))
         if sum(player_cards) == 21:
             print("You have a BlackJack!", player_cards)
-            while sum(dealer_cards) < 16:
+            while sum(dealer_cards) < 17:
                 dealer_cards.append(random.randint(1,11))
                 if sum(dealer_cards) > 21:
                     print("Dealer has busted " + str(sum(dealer_cards)) + " with",dealer_cards)
@@ -60,7 +51,7 @@ while sum(player_cards) < 21:
        
         
     elif action == "stay":
-        while sum(dealer_cards) < 16:
+        while sum(dealer_cards) < 17:
             dealer_cards.append(random.randint(1,11))
         if sum(dealer_cards) > 21:
             print("Dealer has busted " + str(sum(dealer_cards)) + " with",dealer_cards)
